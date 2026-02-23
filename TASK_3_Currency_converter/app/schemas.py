ae94706dd16358202be687a336d35b9001a41d51
Nguruvilu <unluckyblood@mail.ru>
2026-02-23 18:04:04 +0300

    add_code_TASK_3_in_file_schemas_py

---
+++ TASK_3_Currency_converter/app/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field, field_validator
+from datetime import datetime
+from typing import Optional
+
+class ConvertRequest(BaseModel):
+    from_currency: str = Field(..., alias="from", min_length=3, max_length=3, description="Kod started valets (naprimer, RUB)")
+    to_corrency: str = Field(..., alias="to", min_length=3, max_length=3, description="Kod iskomoi valets (naprimer KZT)")
+    amount: float = Field(..., gt=0, description="sum for convert")
+    date: Optional[str] = Field(None, description="data in formate YYYY-MM-DD")
+
+    @field_validator("from_currency", "to_corrency")
+    def validate_currency_code(cls, v):
+        if not v.isalpha() or len(v) !=3:
+            raise ValueError("Код валюты должен иметь кратко оф название состоящее из трёх букв")
+        return v.upper()
+    @field_validator("date")
+    def validate_date(cls, v):
+        if v:
+            try:
+                datetime.strptime(v, "%Y-%m-%d")
+            except ValueError:
+                raise ValueError("Неверный формат даты, используйте YYYY-MM-DD")
+            return v
+class ConvertResponse(BaseModel):
+    from_currency: str
+    to_currency: str
+    amount: float
+    converted: float
+    rate: float
+    last_updated: datetime
+
+class CurrencyListResponse(BaseModel):
+    from_currency: list[str]
