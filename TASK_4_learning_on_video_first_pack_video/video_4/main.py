3ab167af1a4648fdc1652e4547e8ec5b92bcdae2
Nguruvilu <unluckyblood@mail.ru>
2026-03-01 22:04:49 +0300

    4

---
+++ TASK_4_learning_on_video_first_pack_video/video_4/main.py
@@ -0,0 +1,15 @@
+number = 5 #int
+str_num = "5" #str
+digit = -4.5342562662 #float
+word = "Результат: " #string
+
+ril = True #bool
+print (number + digit) #int + float = float
+print (type(number + digit))
+print(word, str(ril))
+print(word, ril)
+print(word, number)
+del number
+
+number = 7
+print("Результат: ", number)
