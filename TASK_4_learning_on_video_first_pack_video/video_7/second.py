b17e373e7d02cd13ae955c036a013cca2975e0a0
Nguruvilu <unluckyblood@mail.ru>
2026-03-19 23:33:23 +0300

    FIX

---
+++ TASK_4_learning_on_video_first_pack_video/video_7/second.py
@@ -0,0 +1,13 @@
+nums = [5,2,7,"50", False]
+
+# print(nums[0])
+# print(nums[1])
+# print(nums[2])
+
+# вывод всех элементов списка
+for el in nums:
+    print(el)
+
+for el in nums:
+    el *= 2 #Если False умножить то будет прост умножаться как число 0 на что-то
+    print(el)
