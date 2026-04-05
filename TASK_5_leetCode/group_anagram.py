f2159978a00fa4029311e64b7ef1263277902cfd
Nguruvilu <unluckyblood@mail.ru>
2026-04-05 21:54:13 +0300

    657_robot

--- TASK_5_leetCode/group_anagram.py
+++ TASK_5_leetCode/group_anagram.py
@@ -28,13 +28,8 @@
 # 0 <= strs[i].length <= 100
 # strs[i] состоит из строчных букв английского алфавита.
 
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        truSpisok = set()
-        for e in range(len(strs)):
-            for i in range(len(strs)):
-                if e == i :
-                    continue
-                if sorted(strs[e]) == sorted(strs[i]):
-                    truSpisok.add()
-                
+def groupAnagrams(strs):
+    d = {}
+    for s in strs:
+        d.setdefault(''.join(sorted(s)), []).append(s)
+    return list(d.values())
