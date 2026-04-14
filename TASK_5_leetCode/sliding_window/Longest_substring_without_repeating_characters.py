3a690a63fb4a8463e91778ee19a5d781f37febf9
Nguruvilu <unluckyblood@mail.ru>
2026-04-14 17:26:45 +0300

    Long_rep

--- TASK_5_leetCode/sliding_window/Longest_substring_without_repeating_characters.py
+++ TASK_5_leetCode/sliding_window/Longest_substring_without_repeating_characters.py
@@ -45,3 +45,26 @@ class Solution:
 sol = Solution()
 s="dvdf"
 print(sol.lengthOfLongestSubstring(s))
+
+# я сделал медленный
+# вот более быстрый
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        window = set()
+        l = 0 
+        res = 0
+        for r in range(len(s)):
+            while s[r] in window:
+                window.remove(s[l])
+                l+=1
+            
+            window.add(s[r])
+
+            res = max(res,r-l+1)
+        
+        return res
+    
+        
+sol = Solution()
+s="dvdf"
+print(sol.lengthOfLongestSubstring(s))
