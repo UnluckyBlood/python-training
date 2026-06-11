b187e7ce5612d9984a4498a403d3f2702645405b
Nguruvilu_PC <inluckyblood@mail.ru>
2026-06-11 21:50:36 +0300

    leet_code_17

---
+++ TASK_5_leetCode/just_random_task/17_Letter_Combination_of_a_Phone_num.py
@@ -0,0 +1,26 @@
+
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        
+        mapping = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+        
+        result = []
+        
+        def backtrack(index: int, path: List[str]):
+            if index == len(digits):
+                result.append(''.join(path))
+                return
+            for letter in mapping[digits[index]]:
+                path.append(letter)         
+                backtrack(index + 1, path)   
+                path.pop()                   
+        
+        backtrack(0, [])
+        return result
